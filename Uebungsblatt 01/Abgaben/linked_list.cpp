d053ee9652eecded2c45cb038280d837a332114e
aylmao <aylmao@gmlamdlkam.com>
2016-04-18 15:42:11 +0200

    LLIST.cpp implementation

---
+++ Uebungsblatt 01/Abgaben/linked_list.cpp
@@ -0,0 +1,30 @@
+
+template<typename T>
+class Node
+{
+public:
+	Node(T* _value);
+	Node(T* _value, Node<T>* _parent);
+	~Node();
+
+	T* getData();
+	Node<T>* getParent();
+	Node<T>* getNext();
+	bool getHasNext();
+	bool getHasParent();
+private:
+	T* m_Data;
+	Node<T>* m_Parent;
+	bool m_HasParent;
+	bool m_HasNext;
+};
+
+
+template<typename T>
+class LinkedList
+{
+public:
+	LinkedList();
+	~LinkedList();
+private:
+};

5ae0b221417eb256ab31297dfc5156f06d313db0
aylmao <aylmao@gmlamdlkam.com>
2016-04-19 12:20:18 +0200

    commenting 60%

--- Uebungsblatt 01/Ex01_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex01/p2/SpeedList.java
+++ Uebungsblatt 01/Ex01_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex01/p2/SpeedList.java
@@ -1,9 +1,8 @@
 package de.unistuttgart.dsass2016.ex01.p2;
 
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class SpeedList<T> implements ISpeedList<T>, Iterable<T> {
+public class SpeedList<T> implements ISpeedList<T>{
 	// TODO: COMMENT THIS SHIT
 	private int speedListSize;
 	// add head
@@ -18,17 +17,44 @@ public class SpeedList<T> implements ISpeedList<T>, Iterable<T> {
 	}
 
 	@Override
+	/**
+	 * inserts a element T at the beginning of the list
+	 * 
+	 */
 	public void prepend(T t) {
 		addFirst(t);
 	}
 
 	@Override
+	/**
+	 * TODO: implement IndexOutOfBoundsException
+	 * 
+	 * Returns the element at the specified position in the list
+	 * 
+	 * @param pos The position of the element in the list starting from 0
+	 * 
+	 * @return The specified element in the list
+	 * 
+	 * @throws IndexOutOfBoundsException If the requested element is out of
+	 * range
+	 */
 	public T getElementAt(int pos) {
 		return get(pos);
 	}
 
 	@Override
 	// TODO: fix and clean up
+	/**
+	 * Returns the next 8th element of the specified element in the list
+	 * 
+	 * @param pos The position of the specified element in the list starting
+	 * from 0
+	 * 
+	 * @return The next 8th element of the specified element
+	 * 
+	 * @throws IndexOutOfBoundsException If the requested element is out of
+	 * range
+	 */
 	public T getNext8thElementOf(int pos) {
 		try {
 			// setting up a tmp head-node
@@ -51,21 +77,33 @@ public class SpeedList<T> implements ISpeedList<T>, Iterable<T> {
 					throw new NoSuchElementException();
 				}
 			}
+			//if the pointer could find an n+8 element it returns it 
 			return last.getData();
 		} catch (NoSuchElementException e) {
+			//if NoSuchElementException gets thrown gets catched here and return null
 			return null;
 		}
 	}
-
+	/**
+	 * checks if the head is null, which when its filled it shouldnt
+	 * @return
+	 */
 	public boolean isEmpty() {
 		return head == null;
 	}
-
+	
+	/**
+	 * transfer this code to prepend() 
+	 * @param t
+	 */
 	public void addFirst(T t) {
 		head = new Node<T>(t, head);
 		speedListSize++;
 	}
-
+	/**
+	 * TODO: maybe implement a getT(); method in class node ?
+	 * @return
+	 */
 	public T getFirst() {
 		if (head == null) {
 			throw new NoSuchElementException();
@@ -73,7 +111,10 @@ public class SpeedList<T> implements ISpeedList<T>, Iterable<T> {
 			return head.t;
 		}
 	}
-
+	/**
+	 * TODO: maybe unnecessary
+	 * @return
+	 */
 	public T removeFirst() {
 		T tmp = getFirst();
 		head = head.next;
@@ -106,12 +147,18 @@ public class SpeedList<T> implements ISpeedList<T>, Iterable<T> {
 			return tmp.t;
 		}
 	}
-
-	public void flush() {
+	/**
+	 * if the head is null we can forget about the rest of the list
+	 */
+	public void dropList() {
 		this.head = null;
 		speedListSize = 0;
 	}
-
+	/**
+	 * 
+	 * @param index
+	 * @return
+	 */
 	public T get(int index) {
 		if (head == null) {
 			throw new IndexOutOfBoundsException();
@@ -133,21 +180,22 @@ public class SpeedList<T> implements ISpeedList<T>, Iterable<T> {
 	// TODO: Node finished
 	@SuppressWarnings("hiding")
 	public class Node<T> {
-
+		//stored information
 		private T t;
-
+		//null node next, also works as TAIL at the end
 		private Node<T> next;
-
+		
+		//constructor
 		public Node(T t, Node<T> next) {
 			this.t = t;
 			this.next = next;
 
 		}
-
+		//
 		public T getData() {
 			return t;
 		}
-
+		//
 		public Node<T> getNext() {
 			if (next != null) {
 				return next;
@@ -155,42 +203,44 @@ public class SpeedList<T> implements ISpeedList<T>, Iterable<T> {
 				return null;
 			}
 		}
-
+		//checks if there exists a next element
 		public boolean hasNext() {
 			return next != null;
 		}
 
 	}
+/**
+ * dont need dont care
+ * 
+ */
+//	public Iterator<T> iterator() {
+//
+//		return new SpeedListIterator();
+//	}
 
-	@Override
-	public Iterator<T> iterator() {
-
-		return new SpeedListIterator();
-	}
-
-	private class SpeedListIterator implements Iterator<T> {
-		private Node<T> nextNode;
-
-		public SpeedListIterator() {
-			nextNode = head;
-		}
-
-		@Override
-		public boolean hasNext() {
-			return nextNode != null;
-		}
-
-		@Override
-		public T next() {
-			if (!hasNext()) {
-				throw new NoSuchElementException();
-			} else {
-				T res = nextNode.t;
-				nextNode = nextNode.next;
-				return res;
-			}
-		}
-
-	}
+//	private class SpeedListIterator implements Iterator<T> {
+//		private Node<T> nextNode;
+//
+//		public SpeedListIterator() {
+//			nextNode = head;
+//		}
+//
+//		@Override
+//		public boolean hasNext() {
+//			return nextNode != null;
+//		}
+//
+//		@Override
+//		public T next() {
+//			if (!hasNext()) {
+//				throw new NoSuchElementException();
+//			} else {
+//				T res = nextNode.t;
+//				nextNode = nextNode.next;
+//				return res;
+//			}
+//		}
+//
+//	}
 
 }

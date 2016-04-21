acc657ece17bf1d316bc4f61997a75469f4c9fb0
Wilhelm B <buch.willi@googlemail.com>
2016-04-21 11:15:29 +0200

    bubblesort progress

--- Uebungsblatt 02/Ex02_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex02/p2/Sorter.java
+++ Uebungsblatt 02/Ex02_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex02/p2/Sorter.java
@@ -1,11 +1,34 @@
 package de.unistuttgart.dsass2016.ex02.p2;
 
 public class Sorter {
-	
+
 	public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
+
 	}
 
 	public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
+		// public static int[] bubblesort(int[] zusortieren) {
+		// int temp;
+		// for(int i=1; i<zusortieren.length; i++) {
+		// for(int j=0; j<zusortieren.length-i; j++) {
+		// if(zusortieren[j]>zusortieren[j+1]) {
+		// temp=zusortieren[j];
+		// zusortieren[j]=zusortieren[j+1];
+		// zusortieren[j+1]=temp;
+		// }
+		//
+		// }
+		// }
+		// return zusortieren;
+		// }
+
+		int temp;
+
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size()-i; j++) {
+				
+			}
+		}
 	}
 
 	public static <T extends Comparable<T>> void shakerSort(ISimpleList<T> list) {

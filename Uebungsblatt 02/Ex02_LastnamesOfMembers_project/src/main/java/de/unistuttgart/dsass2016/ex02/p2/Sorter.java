6431ef33707f7487054b7b14f5ac67d21a70f022
Wilhelm B <buch.willi@googlemail.com>
2016-04-21 10:55:41 +0200

    Übungsblatt 2 entzippt

---
+++ Uebungsblatt 02/Ex02_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex02/p2/Sorter.java
@@ -0,0 +1,14 @@
+package de.unistuttgart.dsass2016.ex02.p2;
+
+public class Sorter {
+	
+	public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
+	}
+
+	public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
+	}
+
+	public static <T extends Comparable<T>> void shakerSort(ISimpleList<T> list) {
+	}
+
+}
---
+++ Uebungsblatt 02/Ex02_LastnamesOfMembers_project/src/test/java/de/unistuttgart/dsass2016/ex02/p2/SorterTest.java
@@ -0,0 +1,14 @@
+package de.unistuttgart.dsass2016.ex02.p2;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+import de.unistuttgart.dsass2016.ex02.p2.ISimpleList;
+import de.unistuttgart.dsass2016.ex02.p2.Sorter;
+
+public class SorterTest {
+
+
+}

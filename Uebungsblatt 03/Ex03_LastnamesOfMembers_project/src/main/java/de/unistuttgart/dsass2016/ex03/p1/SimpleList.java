1957ee8001b1fadd21a2bf3d8ea90d1a69a4570a
Wilhelm B <buch.willi@googlemail.com>
2016-04-28 19:04:06 +0200

    0% progress

--- Uebungsblatt 03/Ex03_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex03/p1/SimpleList.java
+++ Uebungsblatt 03/Ex03_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex03/p1/SimpleList.java
@@ -2,57 +2,86 @@ package de.unistuttgart.dsass2016.ex03.p1;
 
 import java.util.*;
 
-
 public class SimpleList<T extends Comparable<T>> implements ISimpleListIterable<T> {
-	
+
 	/** Do not modify the existing methods and signatures */
 	private final List<T> list;
-	
+
 	public SimpleList() {
 		list = new ArrayList<T>();
 	}
-	
+
 	@Override
 	public void append(T element) {
 		list.add(element);
 	}
-	
+
 	@Override
 	public int size() {
 		return list.size();
+
 	}
-	
+
 	@Override
 	public T get(int index) {
 		return list.get(index);
 	}
-	
+
 	@Override
 	public void swap(int i, int j) {
 		T tmp = list.get(i);
 		list.set(i, list.get(j));
 		list.set(j, tmp);
 	}
+	
 
 	@Override
 	public Iterator<T> iterator() {
-		
-		return new SimpleListIterator<T>();
+		final SimpleList<T> list = this;
+		//TODO REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+		return new Iterator<T>(){
+			
+			int index = 0;
+			@Override
+			public boolean hasNext() {
+				if (list.size()== 0) {
+					return false;
+				}else {
+					
+				}
+				return false;
+			}
+
+			@Override
+			public T next() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+		};
+		//return new SimpleListIterator<T>(list);
 
 	}
 
 	@Override
 	public Iterator<T> skippingIterator(int n) {
-		//TODO skipping iterator
+		// TODO skipping iterator
 		return null;
 	}
-	
+
 	@SuppressWarnings("hiding")
-	class SimpleListIterator<T> implements Iterator<T>{
+	class SimpleListIterator<T> implements Iterator<T> {
+		private SimpleList<? extends T> list;
+
+		public SimpleListIterator(SimpleList<? extends T> list) {
+
+			this.list = list;
+
+		}
 
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
+
 			return false;
 		}
 
@@ -61,11 +90,16 @@ public class SimpleList<T extends Comparable<T>> implements ISimpleListIterable<
 			// TODO Auto-generated method stub
 			return null;
 		}
-		
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
 	}
-	
+
+
 	@SuppressWarnings("hiding")
-	class SimpleListSkippingIterator<T> implements Iterator<T>{
+	class SimpleListSkippingIterator<T> implements Iterator<T> {
 
 		@Override
 		public boolean hasNext() {
@@ -78,7 +112,11 @@ public class SimpleList<T extends Comparable<T>> implements ISimpleListIterable<
 			// TODO Auto-generated method stub
 			return null;
 		}
-		
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
 	}
 
 }

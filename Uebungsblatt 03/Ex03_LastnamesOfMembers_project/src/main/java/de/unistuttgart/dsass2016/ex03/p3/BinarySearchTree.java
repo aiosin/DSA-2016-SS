509752c3f1802cf4f1d5ce4643d29130028dc58f
aiosin <buch.willi@googlemail.com>
2016-05-03 00:31:03 +0200

    i broke it

--- Uebungsblatt 03/Ex03_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex03/p3/BinarySearchTree.java
+++ Uebungsblatt 03/Ex03_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex03/p3/BinarySearchTree.java
@@ -12,31 +12,21 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 
 	@Override
 	public void insert(T t) {
-		// creating a insertion node
 		IBinaryTreeNode<T> insertionNode = new BinaryTreeNode<T>();
 		insertionNode.setValue(t);
-		// check if there are elements in the root node
-		// if there are not: then the root node will be the first node
 		if (root == null) {
 			root = insertionNode;
 			return;
 		} else if (root != null) {
-			// if the root node is not empty then use the recurisveInsertNode
-			// method
-
 			recursiveInsertNode(root, insertionNode);
 			return;
 		} else {
-			// if this happens youre fucked
 			throw new RuntimeException();
 		}
 	}
 
 	private void recursiveInsertNode(IBinaryTreeNode<T> root, IBinaryTreeNode<T> node) {
-		// left right statement
 		if (root.getValue().compareTo(node.getValue()) < 0) {
-			// if we recursively reached our desired end, we simple put the
-			// value at the point and break;
 			if (root.getLeftChild() == null) {
 				root.setLeftChild(node);
 				return;
@@ -98,22 +88,5 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 		return returnList;
 
 	}
-	
-	public static void main(String[] args) {
-		System.out.println("initiating");
-		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
-		System.out.println("beginning");
-		tree.insert(new Integer(2));
-		tree.insert(new Integer(22));
-		tree.insert(new Integer(23));
-		tree.insert(new Integer(12));
-		tree.insert(new Integer(1232));
-		tree.insert(new Integer(212));
-		tree.insert(new Integer(123332));
-		System.out.println(tree.returnBSTinOrder(tree.root).toString());
-		System.out.println("end now");
-		System.out.println("isfull");
-		System.out.println(tree.isFull());
-		System.out.println("is full end");
-	}
+
 }

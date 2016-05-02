e4d339250c344a44ff7ef8e294a4a6539e71a788
Wilhelm B <buch.willi@googlemail.com>
2016-05-02 18:44:03 +0200

    aufgabe 3 cleaned up 30 %

--- Uebungsblatt 03/Ex03_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex03/p3/BinarySearchTree.java
+++ Uebungsblatt 03/Ex03_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex03/p3/BinarySearchTree.java
@@ -12,40 +12,43 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 
 	@Override
 	public void insert(T t) {
-		//creating a insertion node 
+		// creating a insertion node
 		IBinaryTreeNode<T> insertionNode = new BinaryTreeNode<T>();
 		insertionNode.setValue(t);
-		//check if there are elements in the root node
-		//if there are not: then the root node will be the first node
+		// check if there are elements in the root node
+		// if there are not: then the root node will be the first node
 		if (root == null) {
 			root = insertionNode;
 			return;
 		} else if (root != null) {
-			//if the root node is not empty then use the recurisveInsertNode method
-			
+			// if the root node is not empty then use the recurisveInsertNode
+			// method
+
 			recursiveInsertNode(root, insertionNode);
 			return;
 		} else {
-			//if this happens youre fucked
+			// if this happens youre fucked
 			throw new RuntimeException();
 		}
 	}
-	//this method decides where to put the given node in the tree
-	//if the value is lower than the higher node, node gets sent left
-	//if the value is higher than the higher node, node gets sent right
-	public void recursiveInsertNode(IBinaryTreeNode<T> root, IBinaryTreeNode<T> node){
-		//left right statement
-		if (root.getValue().compareTo(node.getValue())< 0) {
-			//if we recursively reached our desired end, we simple put the value at the point and break; 
+
+	// this method decides where to put the given node in the tree
+	// if the value is lower than the higher node, node gets sent left
+	// if the value is higher than the higher node, node gets sent right
+	public void recursiveInsertNode(IBinaryTreeNode<T> root, IBinaryTreeNode<T> node) {
+		// left right statement
+		if (root.getValue().compareTo(node.getValue()) < 0) {
+			// if we recursively reached our desired end, we simple put the
+			// value at the point and break;
 			if (root.getLeftChild() == null) {
 				root.setLeftChild(node);
-				
-				//TODO: find out if return is necessary
+
+				// TODO: find out if return is necessary
 				return;
 			} else {
 				recursiveInsertNode(root.getLeftChild(), node);
 			}
-		//same story for the right side as for the left side
+			// same story for the right side as for the left side
 		} else {
 			if (root.getRightChild() == null) {
 				root.setRightChild(node);
@@ -54,77 +57,51 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 				recursiveInsertNode(root.getRightChild(), node);
 			}
 		}
-		//CRASHING THIS METHOD;
-		//WITH NO SURVIVORS
 		return;
-		
-	}
-	@Override
-	public IBinaryTreeNode<T> getRootNode() {
-		//WAS GETTING ROOT PART OF YOUR PLAN ?
-		return root;
-		//WELL CONGRATULATIONS YOU GOT YOURSELF CAUGHT
+
 	}
 
-	@SuppressWarnings("unused")
+	@Override
+	public IBinaryTreeNode<T> getRootNode() {
+		return root;
+	}
+
 	@Override
 	public boolean isFull() {
-		//maybe put every Node in existance in a LinkedList and check if either both nodes
-		//and not null or have some data
-		//if even only one fails this test we're going to return false
 		if (root == null) {
 			return true;
 		} else {
-			//Getting all the Nodes into an ArrayList
-			for (IBinaryTreeNode<T> node : returnBSTinOrder(root)) {
-				if (nodeQualifiesForFull(node) == true ) {
-					 
-				}else {
+			for (IBinaryTreeNode<T> currentNode : returnBSTinOrder(root)) {
+				if (nodeQualifiesForFull(currentNode) == true) {
+					continue;
+				} else {
 					return false;
 				}
 			}
+			return true;
+
 		}
-		return false;
-		
-		
-		
+
 	}
-	//instead of 
-	public boolean nodeQualifiesForFull(IBinaryTreeNode<T> node){
+
+	public boolean nodeQualifiesForFull(IBinaryTreeNode<T> node) {
 		if (node.getLeftChild() != null || node.getRightChild() != null) {
 			return true;
-		} else if(node.getLeftChild() == null && node.getRightChild() == null) {
+		} else if (node.getLeftChild() == null && node.getRightChild() == null) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	//HORRIBLY INEFFICIENT AND POSSIBLY DANGEROUS AVOID AT ALL COSTS
-	//method scans the branches of a node and returns them all in 
-	public List<IBinaryTreeNode<T>> returnBSTinOrder(IBinaryTreeNode<T> node){
-		//temporary workaroound HORRIBLY INEFFICIENT (O(2n)) out of place
-		Stack<IBinaryTreeNode<T>> s = new Stack<IBinaryTreeNode<T>>();
-		List<IBinaryTreeNode<T>> returnList = new ArrayList<IBinaryTreeNode<T>>();
-		
-		if(node == null){
-			return null;
-		}
-		IBinaryTreeNode<T> currentNode = root;
-		while(!s.isEmpty() || currentNode != null ){
-			if (currentNode !=null) {
-				s.push(currentNode);
-				currentNode = currentNode.getLeftChild();
-			} else {
-				IBinaryTreeNode<T> n = s.pop();
-				returnList.add(n);
-				currentNode = n.getRightChild();
-			}
-		}
-		
-		
-		
-		return returnList;
-		
-	}
 
+	public List<IBinaryTreeNode<T>> returnBSTinOrder(IBinaryTreeNode<T> rootNode) {
+		List<IBinaryTreeNode<T>> returnList = new ArrayList<IBinaryTreeNode<T>>();
+		while (rootNode != null) {
+			returnBSTinOrder(rootNode.getLeftChild());
+			returnList.add(rootNode);
+			returnBSTinOrder(rootNode.getRightChild());
+		}
+		return returnList;
+
+	}
 }

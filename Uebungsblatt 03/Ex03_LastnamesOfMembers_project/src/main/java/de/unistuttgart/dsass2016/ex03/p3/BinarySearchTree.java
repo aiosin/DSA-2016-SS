170243ab25ff9d4e37a82f39dfc63543d56a46e3
aylmao <aylmao@gmlamdlkam.com>
2016-05-04 14:12:38 +0200

    willi stinkt

--- Uebungsblatt 03/Ex03_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex03/p3/BinarySearchTree.java
+++ Uebungsblatt 03/Ex03_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex03/p3/BinarySearchTree.java
@@ -55,7 +55,7 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 		if (root == null) {
 			return true;
 		} else {
-			for (IBinaryTreeNode<T> currentNode : returnBSTinOrder(root)) {
+			for (IBinaryTreeNode<T> currentNode : returnBSTinOrder(root, null)) {
 				if (nodeQualifiesForFull(currentNode) == true) {
 					continue;
 				} else {
@@ -69,24 +69,51 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 	}
 
 	public boolean nodeQualifiesForFull(IBinaryTreeNode<T> node) {
-		if (node.getLeftChild() != null || node.getRightChild() != null) {
+		if (node.getLeftChild() == null && node.getLeftChild() == null) {
 			return true;
-		} else if (node.getLeftChild() == null && node.getRightChild() == null) {
+		} else if (node.getLeftChild() != null && node.getLeftChild() != null) {
 			return true;
-		} else {
-			return false;
-		}
+		} else return false;
 	}
 
-	public List<IBinaryTreeNode<T>> returnBSTinOrder(IBinaryTreeNode<T> rootNode) {
-		List<IBinaryTreeNode<T>> returnList = new ArrayList<IBinaryTreeNode<T>>();
-		while (rootNode != null) {
-			returnBSTinOrder(rootNode.getLeftChild());
-			returnList.add(rootNode);
-			returnBSTinOrder(rootNode.getRightChild());
+	public List<IBinaryTreeNode<T>> returnBSTinOrder(IBinaryTreeNode<T> rootNode, List<IBinaryTreeNode<T>> list) {
+		if (list == null)
+		{
+			list = new ArrayList<IBinaryTreeNode<T>>();
 		}
-		return returnList;
+		if (rootNode != null) {
+			returnBSTinOrder(rootNode.getLeftChild(), list);
+			list.add(rootNode);
+			returnBSTinOrder(rootNode.getRightChild(), list);
+		}
+		return list;
 
+	}
+	
+	public static void main(String[] args) {
+		BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+		
+		System.out.println("start");
+		tree.insert(new Integer (4));
+		tree.insert(new Integer (3));
+		tree.insert(new Integer (5));
+
+		
+		System.out.println("end insert");
+		
+		System.out.println(tree.getRootNode().getValue());
+		
+		System.out.println("start is full");
+		System.out.println(tree.isFull());
+		List<IBinaryTreeNode<Integer>> list = tree.returnBSTinOrder(tree.getRootNode(), null);
+		for(IBinaryTreeNode<Integer> node : list)
+		{
+			System.out.println(node.getValue());
+		}
+		System.out.println("left child: " + tree.getRootNode().getLeftChild().getValue());
+		System.out.println("right child: " + tree.getRootNode().getRightChild().getValue());
+		System.out.println("end of file");
+		System.exit(0);
 	}
 
 }

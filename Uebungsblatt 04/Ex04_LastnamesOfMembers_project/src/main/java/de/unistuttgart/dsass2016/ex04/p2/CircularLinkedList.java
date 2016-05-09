3b3d377457dffc51295e667d726a21e140cc9e24
Wilhelm B <buch.willi@googlemail.com>
2016-05-09 19:58:45 +0200

    ex04.02  node and baisc list implementation

--- Uebungsblatt 04/Ex04_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex04/p2/CircularLinkedList.java
+++ Uebungsblatt 04/Ex04_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex04/p2/CircularLinkedList.java
@@ -1,7 +1,62 @@
 package de.unistuttgart.dsass2016.ex04.p2;
 
-public class CircularLinkedList<T extends Comparable<T>> implements
-		ICircularLinkedList<T> {
+public class CircularLinkedList<T extends Comparable<T>> implements ICircularLinkedList<T> {
 
+	private ILinkedListNode<T> head = new LinkedListNode<T>();
+
+	// do we need this ?
+	@SuppressWarnings("unused")
+	private ILinkedListNode<T> last = new LinkedListNode<T>();
+
+	private int size;
+
+	@Override
+	public void append(T element) {
+		if (size == 0) {
+			ILinkedListNode<T> temp = new LinkedListNode<T>();
+
+			temp.setElement(element);
+			temp.setPrev(temp);
+			temp.setNext(temp);
+
+			head.setNext(temp);
+
+			size++;
+		} else {
+			// instatiating a new node
+			ILinkedListNode<T> temp = new LinkedListNode<T>();
+			// giving the node the data
+			temp.setElement(element);
+
+			// making the circle go round
+			temp.setNext(head.getNext());
+			
+			//getting the last element in the list
+			ILinkedListNode<T> last = head;
+			while (last.getNext() != head) {
+				last = last.getNext();
+			}
+			//setting the last item in the circle list
+			last.setNext(temp);
+			size++;
+		}
+
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO implement
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public ILinkedListNode<T> /* höhöhöhö => */ getHead() {
+		return head;
+	}
 
 }
--- Uebungsblatt 04/Ex04_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex04/p2/LinkedListNode.java
+++ Uebungsblatt 04/Ex04_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex04/p2/LinkedListNode.java
@@ -1,6 +1,46 @@
 package de.unistuttgart.dsass2016.ex04.p2;
 
 public class LinkedListNode<T extends Comparable<T>> implements ILinkedListNode<T>{
+	
+	public LinkedListNode(){
+		
+	}
+	//TODO: if errors occur change back to normal node not interface defined
+	private T data;
+	private ILinkedListNode<T> prev;
+	private ILinkedListNode<T> next;
+	@Override
+	public T getElement() {
+		
+		return this.data;
+	}
+
+	@Override
+	public void setElement(T element) {
+		this.data = element;
+		
+	}
+
+	@Override
+	public ILinkedListNode<T> getNext() {
+		return this.next;
+	}
+
+	@Override
+	public void setNext(ILinkedListNode<T> next) {
+		this.next = next;
+	}
+
+	@Override
+	public ILinkedListNode<T> getPrev() {
+		return this.prev;
+	}
+
+	@Override
+	public void setPrev(ILinkedListNode<T> prev) {
+		this.prev = prev;
+		
+	}
 
 
 }

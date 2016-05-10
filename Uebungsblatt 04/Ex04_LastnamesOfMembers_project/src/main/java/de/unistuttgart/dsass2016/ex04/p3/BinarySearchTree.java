b21d227e2d89d1a7fbfd4bca8e7d5cd2e5f768cc
Wilhelm B <buch.willi@googlemail.com>
2016-05-10 15:23:02 +0200

    wip

--- Uebungsblatt 04/Ex04_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex04/p3/BinarySearchTree.java
+++ Uebungsblatt 04/Ex04_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex04/p3/BinarySearchTree.java
@@ -1,6 +1,8 @@
 package de.unistuttgart.dsass2016.ex04.p3;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchTreeIterable<T> {
 
@@ -34,22 +36,82 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 		return this.root;
 	}
 
+
 	@Override
 	public Iterator<T> iterator(TreeTraversalType traversalType) {
-		// TODO implement this one thing at a time
-		//return in inner anonymous class
-		//TODO: implement 
-		if (traversalType ==TreeTraversalType.PREORDER) {
-			
+		//this has to be final, also you dont want anything asynchronous to happen
+		final IBinarySearchTree<T> dummyList = this;
+		
+		if (traversalType == TreeTraversalType.PREORDER) {
+			return new Iterator<T>() {
+				
+				int index = 0;
+				
+				public List<T> recReturnInorder(IBinarySearchTree<T> list){
+					List<T> returnList = new ArrayList<T>();
+						
+						
+						return null;
+					
+				}
+				
+				public boolean hasNext() {
+					return false;
+				}
+
+				@Override
+				public T next() {
+					return null;
+				}
+				
+				public void remove(){
+					throw new UnsupportedOperationException();
+				}
+			};
 		} else if (traversalType == TreeTraversalType.INORDER) {
+			return new Iterator<T>() {
+
+				@Override
+				public boolean hasNext() {
+					return false;
+				}
+
+				@Override
+				public T next() {
+					return null;
+				}
+			};
 
 		} else if (traversalType == TreeTraversalType.POSTORDER) {
+			return new Iterator<T>() {
+
+				@Override
+				public boolean hasNext() {
+					return false;
+				}
+
+				@Override
+				public T next() {
+					return null;
+				}
+			};
 
 		} else {
 			// LevelOrder
-			
+			return new Iterator<T>() {
+
+				@Override
+				public boolean hasNext() {
+					return false;
+				}
+
+				@Override
+				public T next() {
+					return null;
+				}
+			};
 		}
-		return null;
+
 	}
 
 }

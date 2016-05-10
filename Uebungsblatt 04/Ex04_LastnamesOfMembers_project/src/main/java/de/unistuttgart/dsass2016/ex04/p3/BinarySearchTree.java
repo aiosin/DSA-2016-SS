afc115de0fb1c6fdf2bac23066a9196af616cb59
Wilhelm B <buch.willi@googlemail.com>
2016-05-10 19:57:41 +0200

    ex04.03 ready but not tested

--- Uebungsblatt 04/Ex04_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex04/p3/BinarySearchTree.java
+++ Uebungsblatt 04/Ex04_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex04/p3/BinarySearchTree.java
@@ -2,7 +2,9 @@ package de.unistuttgart.dsass2016.ex04.p3;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchTreeIterable<T> {
 
@@ -36,25 +38,21 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 		return this.root;
 	}
 
-
+	@SuppressWarnings("unused")
 	@Override
 	public Iterator<T> iterator(TreeTraversalType traversalType) {
-		//this has to be final, also you dont want anything asynchronous to happen
+		// TODO very messy code
+		// TODO: cleanup
 		final IBinarySearchTree<T> dummyList = this;
-		
+
 		if (traversalType == TreeTraversalType.PREORDER) {
 			return new Iterator<T>() {
-				
+
 				int index = 0;
-				
-				public List<T> recReturnInorder(IBinarySearchTree<T> list){
-					List<T> returnList = new ArrayList<T>();
-						
-						
-						return null;
-					
-				}
-				
+
+				List<IBinaryTreeNode<T>> iteratorList = returnBSTpreOrder(dummyList.getRootNode(), null);
+
+				// todo this iteratorstuff
 				public boolean hasNext() {
 					return false;
 				}
@@ -63,13 +61,17 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 				public T next() {
 					return null;
 				}
-				
-				public void remove(){
+
+				public void remove() {
 					throw new UnsupportedOperationException();
 				}
 			};
 		} else if (traversalType == TreeTraversalType.INORDER) {
 			return new Iterator<T>() {
+				// TODO this is inorder
+				int index = 0;
+
+				List<IBinaryTreeNode<T>> iteratorList = returnBSTinOrder(dummyList.getRootNode(), null);
 
 				@Override
 				public boolean hasNext() {
@@ -79,11 +81,17 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 				@Override
 				public T next() {
 					return null;
+				}
+
+				public void remove() {
+					throw new UnsupportedOperationException();
 				}
 			};
 
 		} else if (traversalType == TreeTraversalType.POSTORDER) {
 			return new Iterator<T>() {
+				int index = 0;
+				List<IBinaryTreeNode<T>> iteratorList = returnBSTpostOrder(dummyList.getRootNode(), null);
 
 				@Override
 				public boolean hasNext() {
@@ -93,12 +101,18 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 				@Override
 				public T next() {
 					return null;
+				}
+
+				public void remove() {
+					throw new UnsupportedOperationException();
 				}
 			};
 
 		} else {
 			// LevelOrder
 			return new Iterator<T>() {
+				int index = 0;
+				List<IBinaryTreeNode<T>> iteratorList = returnBSTlevelOrder(dummyList.getRootNode(), null);
 
 				@Override
 				public boolean hasNext() {
@@ -109,9 +123,76 @@ public class BinarySearchTree<T extends Comparable<T>> implements IBinarySearchT
 				public T next() {
 					return null;
 				}
+
+				public void remove() {
+					throw new UnsupportedOperationException();
+				}
 			};
 		}
 
+	}
+
+	public List<IBinaryTreeNode<T>> returnBSTinOrder(IBinaryTreeNode<T> node, List<IBinaryTreeNode<T>> list) {
+		if (list == null) {
+			list = new ArrayList<IBinaryTreeNode<T>>();
+		}
+		if (node != null) {
+			returnBSTinOrder(node.getLeftChild(), list);
+			list.add(node);
+			returnBSTinOrder(node.getRightChild(), list);
+		}
+		return list;
+	}
+
+	public List<IBinaryTreeNode<T>> returnBSTpreOrder(IBinaryTreeNode<T> node, List<IBinaryTreeNode<T>> list) {
+		if (list == null) {
+			list = new ArrayList<IBinaryTreeNode<T>>();
+		}
+		if (node != null) {
+			list.add(node);
+			returnBSTpreOrder(node.getLeftChild(), list);
+			returnBSTpreOrder(node.getRightChild(), list);
+		}
+		return list;
+	}
+
+	public List<IBinaryTreeNode<T>> returnBSTpostOrder(IBinaryTreeNode<T> node, List<IBinaryTreeNode<T>> list) {
+		if (list == null) {
+			list = new ArrayList<IBinaryTreeNode<T>>();
+		}
+		if (node != null) {
+			returnBSTpostOrder(node.getLeftChild(), list);
+			returnBSTpostOrder(node.getRightChild(), list);
+			list.add(node);
+		}
+		return list;
+	}
+
+	// eh, not very efficient what this is doing but meh
+	// its also not recursive but w/e
+	public List<IBinaryTreeNode<T>> returnBSTlevelOrder(IBinaryTreeNode<T> node, List<IBinaryTreeNode<T>> list) {
+		if (list == null) {
+			list = new ArrayList<IBinaryTreeNode<T>>();
+			
+		}
+		
+		Queue<IBinaryTreeNode<T>> levelQueue  = new LinkedList<IBinaryTreeNode<T>>();
+		levelQueue.add(node);
+		while(!levelQueue.isEmpty()){
+			IBinaryTreeNode<T> tempNode = levelQueue.poll();
+			list.add(tempNode);
+			if (tempNode.getLeftChild()!= null) {
+				levelQueue.add(tempNode.getLeftChild());
+			}
+			if(tempNode.getRightChild() != null){
+				levelQueue.add(tempNode.getRightChild());
+			}
+			
+			
+		}
+		
+		
+		return list;
 	}
 
 }

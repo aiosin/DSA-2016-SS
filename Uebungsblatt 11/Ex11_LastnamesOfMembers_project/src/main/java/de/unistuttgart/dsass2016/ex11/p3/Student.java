66a0d53684399c2bae6285682045b7d096929a3e
aiosin <buch.willi@googlemail.com>
2016-07-05 22:58:23 +0200

    ex11 begin

---
+++ Uebungsblatt 11/Ex11_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex11/p3/Student.java
@@ -0,0 +1,22 @@
+package de.unistuttgart.dsass2016.ex11.p3;
+
+public class Student implements Runnable  {
+
+	private String name;
+	private Table table;
+	
+	public Student(String name, Table table){
+		this.name = name;
+		this.table = table;
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void startWork(){
+		
+	}
+}

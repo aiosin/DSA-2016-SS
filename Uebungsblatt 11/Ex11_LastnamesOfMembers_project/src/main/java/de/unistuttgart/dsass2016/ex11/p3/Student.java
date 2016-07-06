ec89b052617531478de39e95bdceb7b250b4d571
aylmao <aylmao@gmlamdlkam.com>
2016-07-06 15:17:21 +0200

    ex11 03
    done ?

--- Uebungsblatt 11/Ex11_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex11/p3/Student.java
+++ Uebungsblatt 11/Ex11_LastnamesOfMembers_project/src/main/java/de/unistuttgart/dsass2016/ex11/p3/Student.java
@@ -1,22 +1,44 @@
 package de.unistuttgart.dsass2016.ex11.p3;
 
-public class Student implements Runnable  {
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Student implements Runnable {
 
 	private String name;
 	private Table table;
-	
-	public Student(String name, Table table){
+
+	public Student(String name, Table table) {
 		this.name = name;
 		this.table = table;
-		
+
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		// prewaiting before writing something accesing the
+		try {
+			Thread.sleep(ThreadLocalRandom.current().nextLong(1000));
+		} catch (InterruptedException e) {
+			System.exit(0);
+
+			System.err.println("aylmao");
+		}
+		synchronized (this.table) {
+			table.getPencil();
+			this.table.writeLog(name);
+			System.out.println("aylmao");
+		}
+		try {
+			Thread.sleep(ThreadLocalRandom.current().nextLong(1000));
+		} catch (Exception e) {
+			System.exit(1);
+		}
+		synchronized (table) {
+			table.notifyStudentDone();
+		}
 	}
-	public void startWork(){
-		
+
+	public void startWork() {
+		run();
 	}
 }
